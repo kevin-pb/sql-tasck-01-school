4d6fc41ead07bdedbc9461d03c9f3663ee818a3f
USER <kevinpb.py@gmail.com>
2024-08-06 19:16:29 +0200

    update the hard level solutions

--- level_hard.sql
+++ level_hard.sql
@@ -1,19 +1,19 @@
 use school
 
 /*
-exercise_1
-*/
+ * exercise_1
+ */
 
 create table classes(
-`ìd` int primary key not null auto_increment,
-`name` varchar (10) not null,
-`credits` int not null
+    `ìd` int primary key not null auto_increment,
+    `name` varchar (10) not null,
+    `credits` int not null
 );
 
 create table inscriptions (
-`id_clases` int ,
-`id_students` int 
+    `id_clases` int ,
+    `id_students` int 
 );
  
-ALTER TABLE inscriptions ADD CONSTRAINT FOREIGN KEY (`id_clases`) REFERENCES classes (`ìd` );
+ALTER TABLE inscriptions ADD CONSTRAINT FOREIGN KEY (`id_clases`) REFERENCES classes (`id` );
 ALTER TABLE inscriptions ADD CONSTRAINT FOREIGN KEY (`id_students`) REFERENCES students (`id` );
